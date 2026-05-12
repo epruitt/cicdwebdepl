a8283809d9fb02dcfaa5d2ed837a6647dc237c7f
epruitt <epruitt223@gmail.com>
2026-05-11 23:42:24 -0500

    initial commit

--- lib/webdepl-stack.ts
+++ lib/webdepl-stack.ts
@@ -1,16 +1,47 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { join } from "path";
+import { existsSync } from "fs";
+import { Distribution, OriginAccessIdentity } from "aws-cdk-lib/aws-cloudfront";
+import { S3BucketOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 
 export class WebdeplStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const deploymentBucket = new Bucket(this, "webDeploymentBucket");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'WebdeplQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const uiDir = join(__dirname, "..", "..", "web", "dist");
+    if (!existsSync(uiDir)) {
+      console.warn("UI dir not found: " + uiDir);
+      return;
+    }
+
+    const orginIdentity = new OriginAccessIdentity(
+      this,
+      "OriginAccessIdentity",
+    );
+    deploymentBucket.grantRead(orginIdentity);
+
+    const distribution = new Distribution(this, "webDeploymentDistribution", {
+      defaultRootObject: "index.html",
+      defaultBehavior: {
+        origin: S3BucketOrigin.withOriginAccessIdentity(deploymentBucket, {
+          originAccessIdentity: orginIdentity,
+        }),
+      },
+    });
+
+    new BucketDeployment(this, "webDeployment", {
+      destinationBucket: deploymentBucket,
+      sources: [Source.asset(uiDir)],
+      distribution: distribution,
+    });
+
+    new cdk.CfnOutput(this, "appUrl", {
+      value: distribution.distributionDomainName,
+    });
   }
 }
